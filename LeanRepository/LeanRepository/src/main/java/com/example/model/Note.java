c4443ef32cbe1f552a3a92ad11216c702d0c76cc
DELL <DELL@WINDOWS-FA144MF>
2019-09-01 00:21:48 +0530

    Perfect coding

--- LeanRepository/LeanRepository/src/main/java/com/example/model/Note.java
+++ LeanRepository/LeanRepository/src/main/java/com/example/model/Note.java
@@ -5,91 +5,79 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.validation.constraints.NotBlank;
 
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
+/**
+ * The persistent class for the notes database table.
+ * 
+ */
 @Entity
 @Table(name="notes")
-@EntityListeners(AuditingEntityListener.class)
-@JsonIgnoreProperties(value = {"createAt","updateOn"}, allowGetters = true)
+@NamedQuery(name="Note.findAll", query="SELECT n FROM Note n")
 public class Note implements Serializable {
-	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	@NotBlank
-	private String title;
-	
-	@NotBlank
-	private String description;
-	
-	@Column(nullable = false, updatable = false)
-	@Temporal(TemporalType.TIMESTAMP)
-	@CreatedDate
+	private String id;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name="create_at")
 	private Date createAt;
-	
-	@Column(nullable = false)
-	@Temporal(TemporalType.TIMESTAMP)
-	@LastModifiedDate
+
+	private String description;
+
+	private String title;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name="update_on")
 	private Date updateOn;
 
-	public Long getId() {
-		return id;
+	public Note() {
 	}
 
-	public void setId(Long id) {
+	public String getId() {
+		return this.id;
+	}
+
+	public void setId(String id) {
 		this.id = id;
 	}
 
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
 	public Date getCreateAt() {
-		return createAt;
+		return this.createAt;
 	}
 
 	public void setCreateAt(Date createAt) {
 		this.createAt = createAt;
 	}
 
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getTitle() {
+		return this.title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
 	public Date getUpdateOn() {
-		return updateOn;
+		return this.updateOn;
 	}
 
 	public void setUpdateOn(Date updateOn) {
 		this.updateOn = updateOn;
 	}
 
-	
 }
